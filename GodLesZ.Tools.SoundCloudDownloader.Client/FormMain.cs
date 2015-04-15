10e3544f3abebe07221e7c9f6da0407a2a24c4ea
GodLesZ <godlesz@hotmail.de>
2015-04-15 22:05:43 +0200

    Initial commit

---
+++ GodLesZ.Tools.SoundCloudDownloader.Client/FormMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace GodLesZ.Tools.SoundCloudDownloader.Client {
+
+    public partial class FormMain : MetroForm {
+
+        public FormMain() {
+            InitializeComponent();
+        }
+
+    }
+
+}
